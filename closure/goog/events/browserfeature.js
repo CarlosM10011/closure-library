4ffc90a499ea9df41900f5889fc463ae55a87ebb
Carlos Medrano <crlsmedrano99@gmail.com>
2020-07-06 21:27:39 -0500

    Convert goog.events.BrowserFeature to goog.module

--- closure/goog/events/browserfeature.js
+++ closure/goog/events/browserfeature.js
@@ -8,61 +8,71 @@
  * @fileoverview Browser capability checks for the events package.
  */
 
+goog.module('goog.events.BrowserFeature');
+goog.module.declareLegacyNamespace();
 
-goog.provide('goog.events.BrowserFeature');
+const GoogUserAgent = goog.require('goog.userAgent');
 
-goog.require('goog.userAgent');
-goog.scope(function() {
 
+/**
+ * Tricks Closure Compiler into believing that a function is pure.  The compiler
+ * assumes that any `valueOf` function is pure, without analyzing its contents.
+ *
+ * @param {function(): T} fn
+ * @return {T}
+ * @template T
+ */
+const purify = (fn) => {
+  return ({valueOf: fn}).valueOf();
+};
 
 
 /**
  * Enum of browser capabilities.
  * @enum {boolean}
  */
-goog.events.BrowserFeature = {
+const BrowserFeature = {
   /**
    * Whether the button attribute of the event is W3C compliant.  False in
    * Internet Explorer prior to version 9; document-version dependent.
    */
-  HAS_W3C_BUTTON:
-      !goog.userAgent.IE || goog.userAgent.isDocumentModeOrHigher(9),
+  HAS_W3C_BUTTON: !GoogUserAgent.IE || GoogUserAgent.isDocumentModeOrHigher(9),
 
   /**
    * Whether the browser supports full W3C event model.
    */
   HAS_W3C_EVENT_SUPPORT:
-      !goog.userAgent.IE || goog.userAgent.isDocumentModeOrHigher(9),
+      !GoogUserAgent.IE || GoogUserAgent.isDocumentModeOrHigher(9),
 
   /**
    * To prevent default in IE7-8 for certain keydown events we need set the
    * keyCode to -1.
    */
   SET_KEY_CODE_TO_PREVENT_DEFAULT:
-      goog.userAgent.IE && !goog.userAgent.isVersionOrHigher('9'),
+      GoogUserAgent.IE && !GoogUserAgent.isVersionOrHigher('9'),
 
   /**
    * Whether the `navigator.onLine` property is supported.
    */
   HAS_NAVIGATOR_ONLINE_PROPERTY:
-      !goog.userAgent.WEBKIT || goog.userAgent.isVersionOrHigher('528'),
+      !GoogUserAgent.WEBKIT || GoogUserAgent.isVersionOrHigher('528'),
 
   /**
    * Whether HTML5 network online/offline events are supported.
    */
   HAS_HTML5_NETWORK_EVENT_SUPPORT:
-      goog.userAgent.GECKO && goog.userAgent.isVersionOrHigher('1.9b') ||
-      goog.userAgent.IE && goog.userAgent.isVersionOrHigher('8') ||
-      goog.userAgent.OPERA && goog.userAgent.isVersionOrHigher('9.5') ||
-      goog.userAgent.WEBKIT && goog.userAgent.isVersionOrHigher('528'),
+      GoogUserAgent.GECKO && GoogUserAgent.isVersionOrHigher('1.9b') ||
+      GoogUserAgent.IE && GoogUserAgent.isVersionOrHigher('8') ||
+      GoogUserAgent.OPERA && GoogUserAgent.isVersionOrHigher('9.5') ||
+      GoogUserAgent.WEBKIT && GoogUserAgent.isVersionOrHigher('528'),
 
   /**
    * Whether HTML5 network events fire on document.body, or otherwise the
    * window.
    */
   HTML5_NETWORK_EVENTS_FIRE_ON_BODY:
-      goog.userAgent.GECKO && !goog.userAgent.isVersionOrHigher('8') ||
-      goog.userAgent.IE && !goog.userAgent.isVersionOrHigher('9'),
+      GoogUserAgent.GECKO && !GoogUserAgent.isVersionOrHigher('8') ||
+      GoogUserAgent.IE && !GoogUserAgent.isVersionOrHigher('9'),
 
   /**
    * Whether touch is enabled in the browser.
@@ -118,16 +128,4 @@ goog.events.BrowserFeature = {
   })
 };
 
-
-/**
- * Tricks Closure Compiler into believing that a function is pure.  The compiler
- * assumes that any `valueOf` function is pure, without analyzing its contents.
- *
- * @param {function(): T} fn
- * @return {T}
- * @template T
- */
-function purify(fn) {
-  return ({valueOf: fn}).valueOf();
-}
-});  // goog.scope
+exports = BrowserFeature;
